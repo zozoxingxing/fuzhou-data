6ca32f0c67f6791624071db1899941abb6b8be72
Magic <316063522@qq.com>
2020-09-18 08:05:40 +0800

    commit：优化导航栏切换

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,7 +4,7 @@
  * @Author: Magic
  * @Date: 2020-09-13 18:07:19
  * @LastEditors: Magic
- * @LastEditTime: 2020-09-16 07:39:11
+ * @LastEditTime: 2020-09-18 08:05:02
  */
 import React, { Component } from 'react'
 
@@ -19,47 +19,66 @@ export default class nav extends Component {
                 {
                     id: '#home',
                     name: '首页',
-                    url: '/home'
+                    url: '/home',
+                    offsetTop: 0
                 },
                 {
                     id: '#about',
                     name: '平台介绍',
-                    url: '/home'
+                    url: '/home',
+                    offsetTop: 0
                 },
                 {
                     id: '#experience',
                     name: '产品体验',
-                    url: '/home'
+                    url: '/home',
+                    offsetTop: 0
                 },
                 {
                     id: '#merchant',
                     name: '诚信商家',
-                    url: '/home'
+                    url: '/home',
+                    offsetTop: 0
                 },
                 {
                     id: '',
                     name: '加入我们',
-                    url: '/job'
+                    url: '/job',
+                    offsetTop: 0
                 },
                 {
                     id: '',
                     name: '媒体报道',
-                    url: '/media'
+                    url: '/media',
+                    offsetTop: 0
                 }
             ],
-            navIndex: 0
+            navIndex: 0,
         }
     }
 
-    onChangeNav(id, index) {
+    onChangeNav(item, index) {
         this.setState({
             navIndex: index
         })
-        console.log(this.props, 999)
-        if (id) {
-            let offsetTop = id === '#home' ? 0 : document.querySelector(id).offsetTop;
+        if (item.id) {
+            let offsetTop = item.id === '#home' ? 0 : item.offsetTop ? item.offsetTop : document.querySelector(item.id).offsetTop;
+            const newNavList = this.state.navList.map((current, cIndex) => {
+                if (current.id === item.id) {
+                    item.offsetTop = offsetTop;
+                }
+                return current;
+            })
+            this.setState({
+                navList: newNavList
+            })
             window.scrollTo({
                 top: index > 3 ? 0 : offsetTop,
+                behavior: 'smooth'
+            })
+        } else {
+            window.scrollTo({
+                top: 0,
                 behavior: 'smooth'
             })
         }
@@ -67,7 +86,7 @@ export default class nav extends Component {
 
     render() {
         const listItem = this.state.navList.map((item, index) => {
-            return (<li key={index} className={this.state.navIndex === index ? 'active' : ''} onClick={() => this.onChangeNav(item.id, index)}>
+            return (<li key={index} className={this.state.navIndex === index ? 'active' : ''} onClick={() => this.onChangeNav(item, index)}>
                 <Link to={item.url}>{item.name}</Link>
             </li>)
         })
