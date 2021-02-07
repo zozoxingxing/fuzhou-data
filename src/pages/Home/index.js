4b357304eb8e41f75c43b68e9ed309e1f8b8b67d
hptan <17621306658@163.com>
2021-02-07 17:12:22 +0800

    fix swiper

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
-import React, { useState,useEffect,useCallback } from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import './index.scss'
-import  Swiper from 'swiper/js/swiper.js';
+import Swiper from 'swiper/js/swiper.js';
 import "swiper/css/swiper.min.css"
 
 import Produce from '../../components/product'
@@ -9,74 +9,87 @@ import Unit from '../../components/unit'
 import Plan from '../../components/plan'
 import News from '../../components/news'
 import Slide from '../../components/slide'
-const Home =props=> {
-  const [height, setHeight] = useState(0);
 
-  const onResize = useCallback(() => {
-    setHeight(document.body.clientHeight)
-    console.log('onResize',document.body.clientHeight);
-    
-}, [])
+const Home = props => {
+    const [height, setHeight] = useState(0);
 
-useEffect(() => {
-    setHeight(document.body.clientHeight)
-    window.addEventListener('resize', onResize);
-    return (() => {
-        window.removeEventListener('resize', onResize)
-    })
-}, [])
+    const onResize = useCallback(() => {
+        setHeight(document.body.clientHeight)
+        console.log('onResize', document.body.clientHeight);
 
-const handleScroll = () => {
-  const viewHeight = document.body.clientHeight;// 网页可见区域高
-  const scrollHeight = document.body.scrollHeight;// 浏览器所有内容高度
-  // documentElement 对应的是 html 标签，而 body 对应的是 body 标签。
-  // 当前页面的滚动条纵坐标位置 => document.documentElement.scrollTop
-  // 网页被卷去的高 => document.body.scrollTop
-  const scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-  // 滚动到页面底部
-  if (scrollTop + viewHeight >= (scrollHeight - 50)) {
-      console.log('scroll');
-      
-  }
-}
+    }, [])
+
+    useEffect(() => {
+        setHeight(document.body.clientHeight)
+        window.addEventListener('resize', onResize);
+        return (() => {
+            window.removeEventListener('resize', onResize)
+        })
+    }, [])
+
+    const handleScroll = () => {
+        const viewHeight = document.body.clientHeight;// 网页可见区域高
+        const scrollHeight = document.body.scrollHeight;// 浏览器所有内容高度
+        // documentElement 对应的是 html 标签，而 body 对应的是 body 标签。
+        // 当前页面的滚动条纵坐标位置 => document.documentElement.scrollTop
+        // 网页被卷去的高 => document.body.scrollTop
+        const scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+        // 滚动到页面底部
+        if (scrollTop + viewHeight >= (scrollHeight - 50)) {
+            console.log('scroll');
+
+        }
+    }
 
 // 滚动条添加滚动事件
-useEffect(() => {
-  window.addEventListener('scroll', handleScroll);
-  return () => {
-      // 移除滚动条滚动事件
-      window.removeEventListener('scroll', handleScroll);
-  }
-}, [])
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            // 移除滚动条滚动事件
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
 
-  // useEffect(()=>{
-  //     new Swiper('.swiper-container4', {
-  //       direction: 'vertical',
-  //       slidesPerView: 1,
-  //       mousewheel: true,
-  //     })
-  // },[height])
-      
-return (
-  <div className="page-wrapper">
-    {/* <div className="swiper-container4" style={{height: height - 76, marginTop: 76}}>
-    <div className="swiper-wrapper">
-        <div className="swiper-slide"><Slide/></div>
-        <div className="swiper-slide"><Produce/></div>
-        <div className="swiper-slide"><Recommend/></div>
-        <div className="swiper-slide"><Plan/></div>
-        <div className="swiper-slide"><Unit/></div>
-        <div className="swiper-slide"><News/></div>
-    </div>
-    </div> */}
-    <Slide/>
+    useEffect(() => {
+        new Swiper('.swiper-container4', {
+            direction: 'vertical',
+            slidesPerView: 1,
+            mousewheel: true,
+        })
+    }, [height])
+
+    return (
+        <div className="page-wrapper">
+            <div className="swiper-container4" style={{height: height - 76, marginTop: 76}}>
+                <div className="swiper-wrapper">
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><Slide/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><Produce/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><Recommend/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><Plan/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><Unit/></div>
+                    </div>
+                    <div className="swiper-slide">
+                        <div style={{width: '100%'}}><News/></div>
+                    </div>
+                </div>
+            </div>
+            {/*<Slide/>
     <Produce/>
     <Recommend/>
     <Plan/>
     <Unit/>
-    <News/>
-  </div>
-)
+    <News/>*/}
+        </div>
+    )
 }
 
 export default Home;
