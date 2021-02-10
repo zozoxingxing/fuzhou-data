5731212f5047a92ff646b11ee2555f8529756ac3
zozoxingxing <zozozhangyu@163.com>
2021-02-10 18:45:48 +0800

    nothing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://honganstatic.cls.cn/assets/web/precache-manifest.03b9c77512d79c884031af4e5fa71655.js"
+  "https://honganstatic.cls.cn/assets/web/precache-manifest.87ca5ef886d0fc39eaf3cb98f429219b.js"
 );
 
 self.addEventListener('message', (event) => {
