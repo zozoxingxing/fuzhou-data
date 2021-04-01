df1392503cc09c57646634cfe4b77c9bbc18a71e
zozoxingxing <zozozhangyu@163.com>
2021-04-01 09:23:59 +0800

    解决方案

--- src/components/plan/index.js
+++ src/components/plan/index.js
@@ -29,29 +29,29 @@ export default class Plan extends Component {
     render() {
       const dataList = [
         {
-          img: 'https://honganstatic.cls.cn/assets/web/traffic.png',
-          // img: '../../assets/images/traffic.png',
+          // img: 'https://honganstatic.cls.cn/assets/web/traffic.png',
+          img: require('../../assets/images/traffic.png'),
           title: '交通地理',
           content: '基于交通与地理大数据制定智能决策，帮助城市实现智慧管控、文明交通。'
         }, {
-          img: 'https://honganstatic.cls.cn/assets/web/tax.png',
-          // img: '../../assets/images/tax.png',
+          // img: 'https://honganstatic.cls.cn/assets/web/tax.png',
+          img: require('../../assets/images/tax.png'),
           title: '涉税服务',
           content: '帮助企业实现智能申报，节约人力、提高效率，促进企业数字化转型。'
         },{
-          img: 'https://honganstatic.cls.cn/assets/web/insurance.png',
-          // img: '../../assets/images/insurance.png',
+          // img: 'https://honganstatic.cls.cn/assets/web/insurance.png',
+          img: require('../../assets/images/insurance.png'),
           title: '医保云贷',
           content: '以医保结算业务情况为基础，面向信誉良好、经营稳定的小微企业发放信用贷款。'
         },{
-          img: 'https://honganstatic.cls.cn/assets/web/money.png',
-          // img: '../../assets/images/money.png',
+          // img: 'https://honganstatic.cls.cn/assets/web/money.png',
+          img: require('../../assets/images/money.png'),
           title: '榕闪贷',
           content: '基于福州市公共信用数据分析的信用贷款，促进消费，助力经济恢复。'
         },
         {
-          img: 'https://honganstatic.cls.cn/assets/web/trade.png',
-          // img: '../../assets/images/trade.png',
+          // img: 'https://honganstatic.cls.cn/assets/web/trade.png',
+          img: require('../../assets/images/trade.png'),
           title: '智慧文旅',
           content: '基于大数据监测、统计城市旅游动态指标，助力城市规避风险、智慧管控。'
         },
