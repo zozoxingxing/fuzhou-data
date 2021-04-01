b911712f3d8948134a1f0c2f68ea75b8dc40608d
zozoxingxing <zozozhangyu@163.com>
2021-04-01 09:29:47 +0800

    炫酷大屏

--- src/components/unit/index.js
+++ src/components/unit/index.js
@@ -7,22 +7,22 @@ export default class Unit extends Component {
     render() {
         const dataList = [
           {
-            icon: 'https://honganstatic.cls.cn/assets/web/icon1.png',
+            icon: require('../../assets/images/icon1.png'),
             title: '汇聚价值',
             content: '汇聚多源头数据，提炼数据隐藏价值',
           },
           {
-            icon: 'https://honganstatic.cls.cn/assets/web/icon2.png',
+            icon: require('../../assets/images/icon2.png'),
             title: '多维展现',
             content: '多样化的图表展现形式，酷炫科技感的视觉冲击',
           },
           {
-            icon: 'https://honganstatic.cls.cn/assets/web/icon3.png',
+            icon: require('../../assets/images/icon3.png'),
             title: '有效监控',
             content: '24小时监测可靠数据源，智能预警、规避风险',
           },
           {
-            icon: 'https://honganstatic.cls.cn/assets/web/icon4.png',
+            icon: require('../../assets/images/icon4.png'),
             title: '智慧决策',
             content: '基于历史数据轨迹进行分析和预判，助力智慧决策',
           }
